39b46c2d6f1f26bbee6388dcf5d50136efa6eb89
nolens-volens <atrushkevich.ivan@gmail.com>
2020-08-24 22:45:45 +0300

    fff

--- src/main/java/by/atrushkevich/shape/reader/DataReader.java
+++ src/main/java/by/atrushkevich/shape/reader/DataReader.java
@@ -1,26 +1,34 @@
 package by.atrushkevich.shape.reader;
 
 
+import by.atrushkevich.shape.exception.ProjectException;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class DataReader {
 
     private static final Logger logger = LogManager.getLogger();
 
-   /* public void write(String data) {
-        try {
-            try (FileWriter fw = new FileWriter(new
-                    File("src\\main\\resources\\data\\parameters.txt"))) {
-                fw.write(data);
-            }
+    public List<String> readData(String filePath) {
+        List<String> lines = null;
+        try (FileReader fileReader = new FileReader(filePath);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            lines = bufferedReader.lines().collect(Collectors.toList());
         } catch (IOException e) {
-            logger.fatal("Unable to open file.", e);
+            logger.log(Level.ERROR, "File reading error", e);
+            try {
+                throw new ProjectException("File reading error: " + filePath, e);
+            } catch (ProjectException projectException) {
+                projectException.printStackTrace();
+            }
         }
-    }*/
+        return lines;
+
+    }
 }
