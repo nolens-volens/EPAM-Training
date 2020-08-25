6286746f5873361dd8c8e1c076731edafd5a27f3
nolens-volens <atrushkevich.ivan@gmail.com>
2020-08-25 17:43:46 +0300

    Added standard HTML page tags

--- src/main/java/by/atrushkevich/shape/creator/PointCreator.java
+++ src/main/java/by/atrushkevich/shape/creator/PointCreator.java
@@ -1,6 +1,10 @@
 package by.atrushkevich.shape.creator;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class PointCreator {
 
+    static final Logger logger = LogManager.getLogger();
 
 }
--- src/main/java/by/atrushkevich/shape/creator/QuadrangleCreator.java
+++ src/main/java/by/atrushkevich/shape/creator/QuadrangleCreator.java
@@ -1,6 +1,11 @@
 package by.atrushkevich.shape.creator;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class QuadrangleCreator {
+
+    static final Logger logger = LogManager.getLogger();
 
 
 }
