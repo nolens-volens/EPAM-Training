b112e5282f77669935ffc2c00d47d3bd61382286
nolens-volens <atrushkevich.ivan@gmail.com>
2020-08-25 15:26:42 +0300

    Added standard HTML page tags

--- src/main/java/by/atrushkevich/shape/main/Main.java
+++ src/main/java/by/atrushkevich/shape/main/Main.java
@@ -1,25 +1,19 @@
 package by.atrushkevich.shape.main;
 
-import by.atrushkevich.shape.entity.Point;
+import by.atrushkevich.shape.reader.DataReader;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.Comparator;
 
 public class Main {
 
     static Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
-
-        Point point1 = new Point(2, 5);
-        Point point2 = new Point(7, 3);
-        Comparator<Point> comparator = (o1, o2) -> (int) (o1.getPointId() - o2.getPointId());
-        //TODO
-        Comparator.comparing(Point::getPointId).thenComparing(Point::getCoordinateX);
-        logger.log(Level.INFO, "hell world");
-
+        DataReader dataReader = new DataReader();
+        dataReader.readData("src\\main\\resources\\data\\parameters.txt");
+        logger.log(Level.INFO, "The file was read successfully");
 
     }
 }
