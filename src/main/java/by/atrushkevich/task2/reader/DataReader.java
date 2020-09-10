adc74ad6069a911ea92ca78a9c6c3be0cd087e74
nolens-volens <atrushkevich.ivan@gmail.com>
2020-09-10 16:08:23 +0300

    Added standard HTML page tags

--- src/main/java/by/atrushkevich/task2/reader/DataReader.java
+++ src/main/java/by/atrushkevich/task2/reader/DataReader.java
@@ -5,23 +5,19 @@ import by.atrushkevich.shape.exception.ProjectException;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class DataReader {
     private static final String DATA_PATH = "src/main/resources/data/dataText.txt";
 
-    public List<String> readData(String filePath) throws ProjectException {
-        if (filePath == null || !Files.exists(Paths.get(filePath))) {
-            filePath = DATA_PATH;
-        }
+    public List<String> readData(String DATA_PATH) throws ProjectException {
+
         List<String> dataLines;
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(DATA_PATH))) {
             dataLines = bufferedReader.lines().collect(Collectors.toList());
         } catch (IOException e) {
-            throw new ProjectException("Error while opening file: " + filePath, e);
+            throw new ProjectException("Error while opening file: " + DATA_PATH, e);
         }
         return dataLines;
     }
